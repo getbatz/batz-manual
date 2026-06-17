9189a3497bf24c39834d68bda6f1b636633f301b
getbatz <get.batz@gmail.com>
2026-06-18 01:09:52 +0300

    Create OrderHistory.js

---
+++ components/ui/OrderHistory.js
@@ -0,0 +1,8 @@
+export default function OrderHistory({ lang }) {
+  return (
+    <div style={{ borderTop: '1px solid #eee', paddingTop: 10 }}>
+      <h3 style={{ fontSize: 16, marginBottom: 10 }}>История заказов</h3>
+      <p style={{ color: '#999', fontSize: 14, textAlign: 'center' }}>Пока нет поездок</p>
+    </div>
+  );
+}
